5c85e2e1ae6433f783c7a9cee427207d625a1674
MAOTPE <beaulac27@gmail.com>
2026-08-28 22:03:44 +0800

    Update state.js

--- api/state.js
+++ api/state.js
@@ -1,1 +1,14 @@
+const { readAllClasses } = require('../lib/redis');
 
+module.exports = async (req, res) => {
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'GET only' });
+    return;
+  }
+  try {
+    const { classes, storageMode } = await readAllClasses();
+    res.status(200).json({ classes, storageMode });
+  } catch (err) {
+    res.status(500).json({ error: String((err && err.message) || err) });
+  }
+};
